c3662e2e81026ada1ee33fe409a7d1eb15eb944a
Gabriel Naval <gabrielnaval2001@gmail.com>
2020-03-18 17:21:14 -0400

    starting tutorial 2

---
+++ ENTER THE MAGICKA/src/game/gfx/SpriteSheet.java
@@ -0,0 +1,16 @@
+package game.gfx;
+
+public class SpriteSheet {
+
+    public String path;
+    public int width;
+    public int height;
+
+    public int[] pixels;
+
+    public SpriteSheet(String path){
+        BufferedImage image;
+
+        
+    }
+}
